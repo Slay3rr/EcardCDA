d3d7a6dd284d51783770da43be3762abdbba86b1
ilian <ilian.guettaf@hotmail.fr>
2025-02-28 14:55:51 +0100

    test

--- assets/app.js
+++ assets/app.js
@@ -7,5 +7,9 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './images/Carousel/slide1.jpg';
+import './images/Carousel/slide2.jpg';
+import './images/Carousel/slide3.jpg';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
